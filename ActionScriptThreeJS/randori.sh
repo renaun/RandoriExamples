ac974b754c6590445542f12a609e89ecc6c99cca
Renaun Erickson <renaun@gmail.com>
2013-05-17 10:17:13 -0700

    Updated command line options with new randori compiler sdk-path options

--- ActionScriptThreeJS/randori.sh
+++ ActionScriptThreeJS/randori.sh
@@ -1,17 +1,14 @@
 ## INSTRUCTIONS TO RUN
-# cd into src/
-# run ../randori.sh
+# Open a command prompt and from the RandoriExamples/ActionScriptThreeJS folder run:
+# ./randori.sh
 
 ## PATH VALUES CHANGE FOR YOUR MACHINE
 # Path to Randori Repos
 repos=/Code/WebRelated/RandoriRepos
 # Randori Compiler Path
-rc=$repos/randori-compiler-0.2.4.1
-# Randori SDK
-rswc=$repos/randori-sdk/randori-framework/bin/swc
+rc=$repos/randori-compiler-0.2.4.2
 # Randori Libraries SWC
 rlswc=$repos/randori-libraries/bin/randori-libraries.swc
-###rlswc=$repos/randori-sdk-master/randori-framework/bin/swc/HTMLCoreLib.swc
 # sdk-path
 rsdk=$repos/randori-sdk/
 # Source File Base Path
@@ -22,4 +19,4 @@ output=../js-randori
 # Change into the source folder because of bug
 cd src/
 # Run Randoir Compiler:
-java -jar $rc/randori.jar randori.compiler.clients.Randori -sdk-path=$rsdk -library-path $rswc/builtin.swc -library-path $rswc/randori-guice-framework.swc -library-path $rlswc -source-path $source -js-classes-as-files=true -output $output
+java -jar $rc/randori.jar randori.compiler.clients.Randori -sdk-path=$rsdk -library-path $rlswc -source-path $source -js-classes-as-files=true -output $output
